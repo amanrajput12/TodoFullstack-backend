678b644c6fb01c0bc8beaa147b1702d08f0281b0
AMAN RAJPUT <rajputaman724857@gmail.com>
2023-09-27 01:05:35 +0530

    origin change of cors

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ conncectToDb()
 // middleware
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
   credentials: true,
 }))
 app.use(bodyParser.json())
