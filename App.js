e0e64050562825df8ef6e25cf89e5eb2a5a907f4
AMAN RAJPUT <rajputaman724857@gmail.com>
2023-09-27 02:00:10 +0530

    method update

--- App.js
+++ App.js
@@ -18,6 +18,7 @@ app.use(express.json())
 app.use(cors({
     origin: 'https://webfullstacktodos.netlify.app/',
   credentials: true,
+  methods:"POST"
 }))
 app.use(bodyParser.json())
 app.use(express.urlencoded({extended:true}))
