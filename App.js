92ca384aaf3dc32314c553bf0f99cb9ba994d6e0
AMAN RAJPUT <rajputaman724857@gmail.com>
2023-10-05 12:14:42 +0530

    cors error resolve

--- App.js
+++ App.js
@@ -15,14 +15,7 @@ conncectToDb()
 
 // middleware
 app.use(express.json())
-app.use(cors({
-  origin: 'https://webfullstacktodos.netlify.app/',
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  optionsSuccessStatus: 204,
-  
 
-}));
 app.use(bodyParser.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieparser())
